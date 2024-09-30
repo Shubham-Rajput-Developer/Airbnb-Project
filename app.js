7771d384fff4e301623c3260a18e2f1e20b5bd62
Shubham-Rajput-Developer <shubhamrajput0504@gmail.com>
2024-09-30 13:42:40 +0530

    database initialization

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
 const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+
+main()
+.then(()=>{
+    console.log("connected to DB");
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+}
+
+app.get("/",(req,res)=>{
+    res.send("Hi ,I am root");
+});
+
+app.get("/testListening",async(req,res)=>{
+    let sampleListing =  new Listing({
+        title:"My new vila",
+        description:"by the beach",
+        price:1200,
+        location:"Pune, Maharastra",
+        country:"India",
+    });
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("successfull testing");
+});
+
+app.listen(8080,()=>{
+    console.log("server is listen on port 8080");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
