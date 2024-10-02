f035891331a5680d891e519d772d3d2e25ce15a6
Shubham-Rajput-Developer <shubhamrajput0504@gmail.com>
2024-10-02 23:23:03 +0530

    today done

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 main()
 .then(()=>{
@@ -31,6 +32,15 @@ app.get("/",(req,res)=>{
     res.send("Hi ,I am root");
 });
 
+const validateListing = (req,res,next)=>{
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+};
 //Index route
 app.get("/listings",wrapAsync(async (req,res)=>{
     const allListings = await Listing.find();
@@ -50,10 +60,7 @@ app.get("/listings/:id",wrapAsync(async (req,res)=>{
 }));
 
 //Create route
-app.post("/listings",wrapAsync(async(req,res)=>{
-        if(!req.body.listing){
-            throw new ExpressError(400,"Send Valid Data for listing");
-        }
+app.post("/listings",validateListing,wrapAsync(async(req,res)=>{
         const listing = new Listing(req.body.listing);
         await listing.save();
         res.redirect("/listings"); 
@@ -67,10 +74,7 @@ app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
 }));
 
 //update route
-app.put("/listings/:id",wrapAsync(async(req,res)=>{
-    if(!req.body.listing){
-        throw new ExpressError(400,"Send Valid Data for listing");
-    }
+app.put("/listings/:id",validateListing,wrapAsync(async(req,res)=>{
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect("/listings");
@@ -93,7 +97,7 @@ app.all("*",(req,res,next)=>{
 app.use((err,req,res,next)=>{
     let {statusCode=500,message="Something wrong"} = err;
     res.status(statusCode).render("error.ejs",{message});
-    //res.status(statusCode).send(message);    
+    //res.status(statusCode).send(message);
 });
 
 app.listen(8080,()=>{
