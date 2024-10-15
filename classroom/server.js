542b330038d604c48bbb03c430e7789611f162f9
Shubham-Rajput-Developer <shubhamrajput0504@gmail.com>
2024-10-15 18:40:33 +0530

    Done

--- classroom/server.js
+++ classroom/server.js
@@ -2,33 +2,51 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
-const cookieParser = require("cookie-parser");
-app.use(cookieParser("screatecode"));
+const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
 
-app.get("/",(req,res)=>{
-    console.log(req.cookies);
-    res.send("Hi, I am root");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+let sessionOption = {
+    secret:"Done",
+    resave:false,
+    saveUninitialized:true
+};
+app.use(session(sessionOption));
+app.use(flash());
+app.use((req,res,next)=>{
+    res.locals.successMsg = req.flash("success");
+    res.locals.errorMsg = req.flash("error");
+    next();
 });
 
-app.get("/greet",(req,res)=>{
-    let {Name="Shubh"} = req.cookies;
-    res.send(`Hi, ${Name}`);
+app.get("/register",(req,res)=>{
+    let {name="default"} = req.query;
+    req.session.name = name;
+    if(name==="default"){
+        req.flash("error","user not registered !");
+    }else{
+        req.flash("success","user register successfully!");
+    }
+    res.redirect("/hello");
 });
-
-
-app.get("/getCookies",(req,res)=>{
-    res.cookie("Name","shubham",{signed:true});
-    res.send("HELLOW BHAI");
+app.get("/hello",(req,res)=>{
+    res.render("page.ejs",{name:req.session.name});
 });
+// app.get("/reqcount",(req,res)=>{
+//     if(req.session.count){
+//         req.session.count++;
+//     }else{
+//         req.session.count = 1;
+//     }
+//     res.send(`You send your request ${req.session.count} times`);
+// });
 
-app.get("/verify",(req,res)=>{
-    console.log(req.signedCookies);
-    console.log(req.cookies);
-    res.send("verified");
-});
-app.use("/users",users);
-app.use("/posts",posts);
-
+// app.get("/test",(req,res)=>{
+//     res.send("Test success");
+// });
 
 app.listen(3000,()=>{
     console.log("server is listenning on port 3000");
